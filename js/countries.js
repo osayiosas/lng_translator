874eaf610719b04608846dbe8446de5e0a689127
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2024-04-06 17:12:19 +0100

    translator: completed the project and implemented copy and voice

---
+++ js/countries.js
@@ -0,0 +1,10 @@
+const countries = {
+  "en-GB": "English",
+  "ig-NG": "Igbo",
+  "yo-NG": "Yoruba",
+  "ha-NE": "Hausa",
+  "hu-NG": "Nigerian",
+  "es-ES": "Spanish",
+  "fr-FR": "French",
+  "it-IT": "Italian",
+};
